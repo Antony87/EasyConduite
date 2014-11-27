99bff8c3e0438ef42e8ab22384d604655f4b2c54
Antony87 <antony.fons@antonyweb.net>
2014-11-27 10:38:59 +0100

    implemnent AudioMediaUI and add logger slf4j

--- easyconduite/src/easyconduite/ui/AudioMediaUI.java
+++ easyconduite/src/easyconduite/ui/AudioMediaUI.java
@@ -18,26 +18,75 @@
 package easyconduite.ui;
 
 import easyconduite.objects.AudioMedia;
+import java.io.IOException;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.media.MediaPlayer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
+ * This class encapsulates logics and behaviors about Custom UI Control of an AudioMedia.
  *
  * @author A Fons
  */
 public class AudioMediaUI {
 
     private FlowPane pane;
-    
+
     private MediaPlayer player;
+
+    final private AudioMedia audioMedia;
     
-    private AudioMedia audioMedia;
-    
-    public AudioMediaUI(final Scene scene,final AudioMedia audioMedia) {
+    private final Logger logger = LoggerFactory.getLogger(AudioMediaUI.class);
+
+    private final static String FXML_CUSTOM_PATH = "ressources/piste.fxml";
+
+    /**
+     * Constructor du UI custom control for an AudioMedia.
+     *
+     * @param scene
+     * @param audioMedia
+     */
+    public AudioMediaUI(final Scene scene, final AudioMedia audioMedia) {
+        
+        logger.debug("Create AudioMedia with {}",audioMedia.getAudioFile().getPath());
+        
         this.audioMedia = audioMedia;
     }
-    
-    
+
+    /**
+     * Add to a scene the custom control UI for an {@link AudioMediaUI}.
+     *
+     * @param scene
+     */
+    public void addUI(final Scene scene) {
+
+        final FXMLLoader loader = new FXMLLoader(getClass().getResource(FXML_CUSTOM_PATH));
+
+        pane = null;
+        try {
+            pane = loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        HBox table = (HBox) scene.lookup("#table");
+        table.getChildren().add(0, pane);
+
+    }
+
+    public MediaPlayer getPlayer() {
+        return player;
+    }
+
+    private void setPlayer(MediaPlayer player) {
+        this.player = player;
+    }
+
+    public AudioMedia getAudioMedia() {
+        return audioMedia;
+    }
 
 }
