e74e118685e2e44b45cd83f9efae4640e578881c
AF <antony@192.168.1.23>
2017-02-01 17:36:13 +0100

    add xml adapter

--- easyconduite/src/main/java/easyconduite/objects/AudioMedia.java
+++ easyconduite/src/main/java/easyconduite/objects/AudioMedia.java
@@ -28,16 +28,18 @@ import javafx.beans.property.ReadOnlyDoubleWrapper;
 import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.beans.property.ReadOnlyStringWrapper;
 import javafx.beans.property.StringProperty;
-
 import javafx.scene.input.KeyCode;
+import javafx.util.Duration;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlTransient;
 
 /**
- * Class POJO that encapsulate AudioMedia behavior.
+ * Class POJO that encapsulate AudioMedia behavior.<br>
+ * This class instantiats an marshallable/unmarsallable object for JAXB.
  *
  * @author A.Fons
+ * @version 1.1
  *
  */
 @XmlAccessorType(XmlAccessType.PROPERTY)
@@ -48,6 +50,12 @@ public class AudioMedia implements Comparable<AudioMedia> {
     private File audioFile;
 
     private String filePathName;
+    
+    private Duration audioDuration=Duration.ONE;
+    
+    private Duration fadeInDuration;
+    
+    private Duration fadeOutDuration;
 
     public final BooleanProperty repeatable = new ReadOnlyBooleanWrapper();
 
@@ -61,40 +69,66 @@ public class AudioMedia implements Comparable<AudioMedia> {
      * Default constructor, to be conform bean specification.
      */
     private AudioMedia() {
-        
     }
 
-    public AudioMedia(File file) {
+    /**
+     * Constructs an AudioMedia object with audio audioFile.
+     *
+     * @param audioFile audio audioFile (*.wav, *.mp3) Depends on platform type.
+     */
+    public AudioMedia(File audioFile) {
         // set default values;
-        this.audioFile = file;
-        this.filePathName = file.getAbsolutePath();
+        this.audioFile = audioFile;
+        this.filePathName = audioFile.getAbsolutePath();
         setRepeatable(Boolean.FALSE);
-        setVolume(0.5);
-        Path path = file.toPath();
+        setVolume(50.0);
+        Path path = audioFile.toPath();
         setName(path.getFileName().toString());
     }
 
-    ////////////////////////////////////////////////////////////////////////////
-    //                        Acesseurs
-    ////////////////////////////////////////////////////////////////////////////
+    /**
+     * Get Unique id.
+     *
+     * @return
+     */
     public UUID getUniqueId() {
         return uniqueId;
     }
 
-    ////////////////////////////////////////////////////////////////////////////
+    /**
+     * Set the audio file (*.wav or *.mp3).<br>
+     * It's XmlTransient cause not able to be xml serialized.
+     *
+     * @param audioFile
+     */
     @XmlTransient
     public void setAudioFile(File audioFile) {
         this.audioFile = audioFile;
     }
 
+    /**
+     * Return the audio file.
+     *
+     * @return
+     */
     public File getAudioFile() {
         return audioFile;
     }
 
+    /**
+     * Return path of the audio file.
+     *
+     * @return
+     */
     public String getFilePathName() {
         return filePathName;
     }
 
+    /**
+     * Set audio file's path.
+     *
+     * @param filePathName
+     */
     public final void setFilePathName(String filePathName) {
         this.filePathName = filePathName;
     }
@@ -102,36 +136,107 @@ public class AudioMedia implements Comparable<AudioMedia> {
     ////////////////////////////////////////////////////////////////////////////
     //                        JavaFX Properties
     ////////////////////////////////////////////////////////////////////////////
+    /**
+     * Return Audio media's repeatability.
+     *
+     * @return
+     */
     public Boolean getRepeatable() {
         return repeatable.getValue();
     }
 
-    public final void setRepeatable(Boolean b) {
-        this.repeatable.setValue(b);
+    /**
+     * Set Audio media repeatability's.
+     *
+     * @param repeatability true or false if the audio file must have
+     * repeatability playing.
+     */
+    public final void setRepeatable(Boolean repeatability) {
+        this.repeatable.setValue(repeatability);
     }
 
+    /**
+     * Observable property for repeatability.
+     *
+     * @return
+     */
     public BooleanProperty repeatableProperty() {
         return repeatable;
     }
     ////////////////////////////////////////////////////////////////////////////
 
+    /**
+     * Return playing volume's of audio (from 0 to 100).<br> 
+     * Default is 50.
+     *
+     * @return
+     */
     public Double getVolume() {
         return volume.getValue();
     }
 
+    /**
+     * Set playing's volume of audio.<br>
+     * If volume > 100 then équal 100, if volume < 0 then 0.
+     * @param volume 
+     */
     public final void setVolume(Double volume) {
+        if(volume<0d){
+            volume=0d;
+        }
+        if(volume>100d){
+            volume=100d;
+        }
         this.volume.setValue(volume);
     }
 
+    public Duration getAudioDuration() {
+        return audioDuration;
+    }
+
+    public void setAudioDuration(Duration audioDuration) {
+        this.audioDuration = audioDuration;
+    }
+
+    public Duration getFadeInDuration() {
+        return fadeInDuration;
+    }
+
+    public void setFadeInDuration(Duration fadeInDuration) {
+        this.fadeInDuration = fadeInDuration;
+    }
+
+    public Duration getFadeOutDuration() {
+        return fadeOutDuration;
+    }
+
+    public void setFadeOutDuration(Duration fadeOutDuration) {
+        this.fadeOutDuration = fadeOutDuration;
+    }
+    
+    
+
+    /**
+     * Observable property for audio volume.
+     * @return 
+     */
     public DoubleProperty volumeProperty() {
         return volume;
     }
     ////////////////////////////////////////////////////////////////////////////
 
+    /**
+     * Return the name of Audio Media object.
+     * @return 
+     */
     public String getName() {
         return name.get();
     }
 
+    /**
+     * Set Audio Media object name's.
+     * @param name 
+     */
     public final void setName(String name) {
         this.name.set(name);
     }
@@ -192,7 +297,7 @@ public class AudioMedia implements Comparable<AudioMedia> {
 
     @Override
     public String toString() {
-        return "AudioMedia{" + "uniqueId=" + uniqueId + ", filePathName=" + filePathName + ", name=" + name.getValue() + ", keycode=" + keycode.getValue().getName() + '}';
+        return "AudioMedia{" + "uniqueId=" + uniqueId + ", filePathName=" + filePathName + ", name=" + name.getValue() + ", keycode=" + keycode.getValue() + '}';
     }
 
     @Override
