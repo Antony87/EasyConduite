0b65c15b546381cf80df900cbad61f537e40790f
AF <antony@192.168.1.23>
2017-02-04 00:45:08 +0100

    Refactoring for less dependencies to controller

--- easyconduite/src/main/java/easyconduite/Easyconduite.java
+++ easyconduite/src/main/java/easyconduite/Easyconduite.java
@@ -16,18 +16,14 @@
  */
 package easyconduite;
 
-import easyconduite.controllers.EasyconduiteController;
-import easyconduite.ui.Chrono;
-import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.scene.image.Image;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
-import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 /**
  *
@@ -54,16 +50,10 @@ public class Easyconduite extends Application {
 
         final FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/easyconduite.fxml"));
 
-        Parent root = loader.load();
+        Pane root = loader.load(); 
         Scene scene = new Scene(root);
 
-        EasyconduiteController controller = loader.getController();
-
         scene.getStylesheets().add("/styles/Styles.css");
-        Label ltimer = (Label) scene.lookup("#timer");
-        Timeline timeline = Chrono.getTimeline(ltimer);
-        controller.setTimeline(timeline);
-        controller.setScene(scene);
         stage.setTitle("EasyConduite 1.1");
         stage.setScene(scene);
         stage.show();
