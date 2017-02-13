706617e9048b0a77dbcc91eea6204bf182c5bb62
Antony87 <antony.fons@antonyweb.net>
2017-02-13 10:31:34 +0100

    add javadoc

--- easyconduite/src/main/java/easyconduite/util/Const.java
+++ easyconduite/src/main/java/easyconduite/util/Const.java
@@ -33,33 +33,61 @@ import javafx.scene.paint.Color;
 import javafx.util.Duration;
 
 /**
+ * This class contains severals application's constants
  *
  * @author antony fons
  */
 public class Const {
 
+    /**
+     * Background color for AudioMedia UI when playings.
+     */
     public static final Background PLAY_BACKG = new Background(new BackgroundFill(Color.web("#455473"), CornerRadii.EMPTY, Insets.EMPTY));
-    
+
+    /**
+     * Background color when AudioMedia UI is paused.
+     */
     public static final Background PAUSE_BACKG = new Background(new BackgroundFill(Color.web("#4c576c"), CornerRadii.EMPTY, Insets.EMPTY));
 
+    /**
+     * Background color when AudioMedia UI is stopped.
+     */
     public static final Background STOP_BACKG = new Background(new BackgroundFill(Color.web("#535965"), CornerRadii.EMPTY, Insets.EMPTY));
 
+    /**
+     * Image for the repeat image icon.
+     */
     public static final Image REPEAT_IMAGE = new Image(AudioMediaUI.class.getResourceAsStream("/icons/repeat.png"), 18, 18, true, false);
 
+    /**
+     * Icon play image path.
+     */
     public static final String NAME_ICON_PLAY = "/icons/PlayGreenButton.png";
 
+    /**
+     * Icon pause image path.
+     */
     public static final String NAME_ICON_PAUSE = "/icons/PauseBlueButton.png";
 
+    /**
+     * Shadow effect applied when audio's playing.
+     */
     public static final Effect SHADOW_EFFECT = new DropShadow(5d, 2d, 2d, Color.BLACK);
 
+    /**
+     * Blomm effect applied to key label when audio's playing.
+     */
     public static final Effect KEYCODE_LABEL_BLOOM = new Bloom(0.4);
 
     public static final TimeZone TZ = TimeZone.getTimeZone("UTC");
 
-    public Const() {
-
-    }
-
+    /**
+     * Format a Duration with SimpleDateFormat.
+     *
+     * @param duration
+     * @param formater
+     * @return
+     */
     public static String getFormatedDuration(Duration duration, SimpleDateFormat formater) {
         return formater.format(new Date((long) duration.toMillis()));
     }
