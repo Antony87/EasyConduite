5291b3b40dcffc0a2e4c001b9349097bf443eae4
antony <antony@antony-W250HUQ>
2017-09-17 22:20:59 +0200

    developpement de l'installeur linux basé sur makeself.sh

--- easyconduite-assembly/src/main/resources/scripts/easyconduite.sh
+++ easyconduite-assembly/src/main/resources/scripts/easyconduite.sh
@@ -8,17 +8,5 @@
 # antony.fons@antonyweb.net
 #########################################################
 
-DIALOG=${DIALOG=dialog}
-destDir=$HOME
-appliDir=$HOME"/Easyconduite"
-
-# check for Java 1.8
-if ! java -version 2>&1 >/dev/null | grep -q "1.8" ; 
-then
-	$DIALOG --title "Avertissement" --msgbox "Impossible de lancer Easyconduite-${project.version} car Java 1.8 ou sup. n'est pas installé"  14 48
-	clear
-	exit 0
-fi
-
 java -Xms512M -Xmx512M -jar ./bin/easyconduite-${project.version}.jar 2>&1 >/dev/null
 exit 0
--- easyconduite-assembly/src/main/resources/scripts/install_easyconduite.sh
+++ easyconduite-assembly/src/main/resources/scripts/install_easyconduite.sh
@@ -1,68 +1,86 @@
 #!/bin/bash
 #########################################################
-# This script executes Easyconduite-1.1.
-# It supposes that the jar is located within :
-# $HOME/Easyconduite-1.1/bin/
+# This script executes Easyconduite-${project.version}
+# 
+# 
 #
 # Antony Fons 2017
 # antony.fons@antonyweb.net
 #########################################################
 
-#DIALOG=${DIALOG=dialog}
+# get system language
+langue=$LANGUAGE
+# get home's owner
+homeOwner=$(find $HOME -maxdepth 0 -printf '%u')
+# import internationalization bundles
+source ./i18_linux/bundle_$langue.conf
 
-#XDialog ckecking
-if [ -z $DISPLAY ]
-then
-DIALOG=dialog
-else
-DIALOG=Xdialog
-fi
-
-
-destDir=$HOME
-
+# Functions
 installer () {
 # TODO
-echo Appel installer $1
+echo -e "\nAppel installer"
 }
-
-cancelMsg () {
-$DIALOG --cr-wrap --title "Avertissement" --msgbox \
-			"\n\nVous avez stopper l'installation de Easyconduite-1.1"  14 48
-clear
-exit 0
-}
-
-# check for Java 1.8
-if ! java -version 2>&1 >/dev/null | grep -q "1.9" ; 
+messageJava(){
+echo -e '\033[1;33;40m'"$alertJava"'\033[0m'
+read -p "$confirm" -n 1 r
+if [ $r != "y" ] && [ $r != "Y" ]
 then
-	$DIALOG --title "Avertissement" --colors --msgbox \
-			"\n\nImpossible d'installer Easyconduite-1.1 car \n\Z1Java 1.8 ou sup. n'est pas présent sur votre ordinateur\Zn"  14 48
-	clear
+	echo -e "\n$cancelInstall"
 	exit 0
 fi
-$DIALOG --cr-wrap --title "Installation EasyConduite-1.1 pour Linux" --yesno \
-			"\n\nBienvenue dans l'installation de \nEasyconduite-1.1 ?\
-			\n\nVoulez-vous poursuivre" 14 48
-			
-if [ $? = "0" ]
-	then
-		DESTREP=`$DIALOG --stdout --title "Choisissez un répertoire de destination" --dselect $destDir/ 14 48`
+}
 
-		case $? in
-			0)
-				installer $DESTREP;;
-			1)
-				cancelMsg;;
-			255)
-				cancelMsg;;
-		esac
-	else
-		cancelMsg
+
+printf '\033[8;40;80t'
+clear
+echo -e '\033[1;36;40m'
+echo $bar
+echo "$title"
+echo "$description"
+echo "$copyright"
+echo $bar
+echo -e '\033[0m'
+echo "$informInstall ($HOME)"
+
+
+#resize window terminal to 40 lines, 100 car.
+#printf '\033[8;40;100t'
+
+#dilaog library ckeching
+#command -v dialog 2>&1 >/dev/null
+#if [ $? = "1" ]
+#then
+#		echo -e '\033[1;33;40m' $dialogrequired '\033[0m'
+#		read -p "désirez-vous continuer (dialog sera installée) y/n ?" -n 1 r
+#		echo -e '\n'$r
+#fi
+
+destDir=$HOME
+# installation confirmation
+read -p "$confirm" -n 1 r
+if [ $r = "y" ] || [ $r = "Y" ]
+then
+	installer
+else
+	echo -e "\n$cancelInstall"
+	exit 0
 fi
 
-	
+#check for java
+command -v java 2>&1 >/dev/null
+if [ $? = "1" ]
+then
+	messageJava
+else
+	java -version
+fi
 
+if ! java -version 2>&1 >/dev/null | grep -q "1.8" ; 
+then
+	echo -e "\n"	
+	messageJava
+fi
 
-#java -Xms512M -Xmx512M -jar $appliDir/bin/easyconduite-1.1-SNAPSHOT.jar &
+cp ./easyconduite.bat $HOME
+#java -Xms512M -Xmx512M -jar $appliDir/bin/easyconduite-${project.version}.jar &
 exit 0
