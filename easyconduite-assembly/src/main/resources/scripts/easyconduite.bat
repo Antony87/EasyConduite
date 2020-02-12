707fcdb05e9cfa1d9333ac6350f0641514796286
V902832 <antony.fons@carsat-centreouest.fr>
2020-02-12 12:40:44 +0100

    Refactoring complet de l'assembly

--- easyconduite-assembly/src/main/resources/scripts/easyconduite.bat
+++ easyconduite-assembly/src/main/resources/scripts/easyconduite.bat
@@ -1,7 +1,7 @@
 rem lancement easyconduite windows
 
-set PATH_TO_EXE_JAVA=C:\Users\V902832\Documents\OpenJDK11U-jdk_x64_windows_hotspot_11.0.4_11\jdk-11.0.4+11\bin\java.exe
+set PATH_TO_JAVA=C:\Users\V902832\Documents\jdk-11.0.6+10-jre
 set PATH_TO_JAVAFX=.\lib
 
-%PATH_TO_EXE_JAVA% -Djava.library.path=C:\tmp --module-path %PATH_TO_JAVAFX% --add-modules javafx.controls,javafx.graphics,javafx.fxml,javafx.media -jar easyconduite-${project.version}-win.jar
+%PATH_TO_JAVA%\bin\java.exe -Djava.library.path=C:\tmp --module-path %PATH_TO_JAVAFX% --add-modules javafx.controls,javafx.graphics,javafx.fxml,javafx.media -jar easyconduite-${project.version}-win.jar
 
